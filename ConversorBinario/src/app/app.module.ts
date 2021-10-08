b92a17b461a7c922cb1cd00f47a734bc4abf43cf
Raul Correa <raulguilherme96@live.com>
2021-10-07 23:31:46 -0300

    versao inicial
    Inclusao de Bootstrap e JSS

---
+++ ConversorBinario/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ConversorBinarioComponent } from './conversorBinario/conversorBinario.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConversorBinarioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ConversorBinario/src/app/conversorBinario/conversorBinario.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-conversorBinario', //seletor entrara em uso na chamada do HTML e TypeScript
+    templateUrl:'./conversorBinario.component.html',
+})
+
+export class ConversorBinarioComponent{} //vincular ao app.modules sempre!
