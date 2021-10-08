89a949d72439995ddefdf4e012e16441a5195b8b
Raul Correa <raulguilherme96@live.com>
2021-10-08 01:14:53 -0300

    Estilização parte1
    - Declaração de variáveis
    
    - Chamada de métodos
    
    - Inclusão do FormsModule no typescript do conversorBinario.component.ts e outros
    
    - Estilização de botão
    
    - Criação de cabecalho usando o comando no terminal
    ˜ng c  cabecalho˜
    
    -Comando ng add @angular/material -> para poder realizar os inserts dos dados
    
    ***Todos componentes devem estar listados no modules...

--- ConversorBinario/src/app/app.module.ts
+++ ConversorBinario/src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; //utilizado para captura/armazenamento de variaveis atraves do html
+
 import { ConversorBinarioComponent } from './conversorBinario/conversorBinario.component';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input'; //utilizado para insercao de variaveis atraves do html
+import {MatCardModule} from '@angular/material/card'; //utilizado para criacao de conteiners
+import {MatButtonModule} from '@angular/material/button'; // para estilização dos botões
+import {MatToolbarModule} from '@angular/material/toolbar' // para estilização do cabecalho/barra de ferramentas
+
+import { CabecalhoComponent } from './cabecalho/cabecalho.component' //usando o comando ˜ng c cabecalho˜ ele cria automaticamente aqui!!!!
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConversorBinarioComponent
+    ConversorBinarioComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, //declarei manualmente
+    BrowserAnimationsModule,
+    MatInputModule, //declarei manualmente
+    MatCardModule, //declarei manualmente
+    MatButtonModule, //declarei manualmente
+    MatToolbarModule //declarei manualmente
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
+})  
 export class AppModule { }
--- ConversorBinario/src/app/conversorBinario/conversorBinario.component.ts
+++ ConversorBinario/src/app/conversorBinario/conversorBinario.component.ts
@@ -1,11 +1,19 @@
 import { Component } from "@angular/core";
 
+
 @Component({
-    selector: 'app-conversorBinario', //seletor entrara em uso na chamada do HTML e TypeScript
-    templateUrl:'./conversorBinario.component.html',
+    selector: 'app-conversorBinario', //seletor entrara em uso na chamada do HTML, TypeScript, CSS, etc..
+    templateUrl:'./conversorBinario.component.html', //apontando o html principal, pode substituir link pelo
 })
 
 export class ConversorBinarioComponent{ //vincular ao app.modules sempre!
+    //definindo variaveis
+    numBinario: string;
+
+    constructor(){
+        this.numBinario = " ";
+    }
+
 
     onConverterBinario(){ //criando um metodo
         console.log('Convertendo valor binario...');
